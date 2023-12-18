d338a80bba902b08e5b0bd0773a592912ffb104e
Muhammad Irfan <mirfan8868@gmail.com>
2023-12-19 03:06:49 +0500

    changes v1

--- config/firebase.js
+++ config/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDY5odWYUXksgUQOpLScVFRxfUk6gV0ZJY",
-  authDomain: "unvoapp-464aa.firebaseapp.com",
-  projectId: "unvoapp-464aa",
-  storageBucket: "unvoapp-464aa.appspot.com",
-  messagingSenderId: "923607384596",
-  appId: "1:923607384596:web:b4f58fa74907f0b2c35d01",
-  measurementId: "G-H6LJ1EFRF0",
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX",
+  measurementId: "XXX",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
-// const app = initializeApp(firebaseConfig);
 export { app };
